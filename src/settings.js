ba61ff05ff237ac7203dfeb9285fce14525122e3
Makoto Wakabayashi <makoto.wakabayashi@access-company.com>
2019-03-20 22:28:33 +0900

    一回目無料の探索的試験。

--- src/settings.js
+++ src/settings.js
@@ -19,12 +19,12 @@ const tours = {
     'ビューアー':0,
     'レンタルした際の動作確認':0,
     'チュートリアルをみながらアプリがそのとおりに動作しているかどうか':0,
-    'アプリ起動時、動画再生':10
+    '一回目無料、古いアプリから対応版への上書きインストール、TOP画面動的対応、ジャンプ画面でのバナー':10
 }
 
 // バックグラウンドの設定
 // 試験するアプリで知っておくべき情報などを記載。
-const background = "https://acsmine.tok.access-company.com/redmine/versions/5650"
+const background = "https://acsmine.tok.access-company.com/redmine/projects/publus-client-v2_jplus/issues?utf8=%E2%9C%93&set_filter=1&sort=id%3Adesc&f%5B%5D=status_id&op%5Bstatus_id%5D=o&f%5B%5D=fixed_version_id&op%5Bfixed_version_id%5D=%3D&v%5Bfixed_version_id%5D%5B%5D=5121&v%5Bfixed_version_id%5D%5B%5D=5715&v%5Bfixed_version_id%5D%5B%5D=5720&f%5B%5D=&c%5B%5D=tracker&c%5B%5D=subject&c%5B%5D=status&c%5B%5D=priority&c%5B%5D=assigned_to&c%5B%5D=due_date&c%5B%5D=updated_on&group_by=&t%5B%5D="
 
 // フォーム入力先の Google Form への URL
 const FORMURL = "https://docs.google.com/forms/d/e/1FAIpQLSfYAQA81O9-Rly-OOMmpwM4Mt7MB5OVbhcEjE3_dSjGJH3EYg/viewform"
